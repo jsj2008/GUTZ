827acf0a7348c9bcd449d7ad56c9b17cbf719ad7
Matt Holcombe (gullinbursti) <code@gullinbursti.cc>
2011-08-29 18:29:11 -0700

    adding model class for game configuration

---
+++ 02_DEV/iPhone App/GutzTest/Classes/model/GameSettingsModel.h
@@ -0,0 +1,20 @@
+//
+//  GameSettingsModel.h
+//  GutzTest
+//
+//  Created by Matthew Holcombe on 08.29.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameSettingsModel : NSObject {
+	
+	
+	
+}
+
+
+@property (nonatomic) BOOL isSoundEnabled;
+
+@end
---
+++ 02_DEV/iPhone App/GutzTest/Classes/model/GameSettingsModel.m
@@ -0,0 +1,24 @@
+//
+//  GameSettingsModel.m
+//  GutzTest
+//
+//  Created by Matthew Holcombe on 08.29.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameSettingsModel.h"
+
+@implementation GameSettingsModel
+
+@synthesize isSoundEnabled;
+
+- (id)init {
+	
+    if ((self = [super init])) {
+    }
+    
+	
+    return (self);
+}
+
+@end
